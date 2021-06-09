a400d00952e8f9b024881cfea95326a3769e5087
vatansoni <vatansoni89@gmail.com>
2021-06-09 21:55:26 +0530

    solution added

---
+++ ProducerconsumerRabbitMQ/Consumer/Receiver.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTestVatan", false, false, false, null);
+
+                //using RabbitMQ.Client.Events;
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    var body = e.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("Message >>>", message);
+                };
+                channel.BasicConsume("BasicTestVatan", true, consumer);
+                Console.WriteLine("Press [enter] to exit");
+            }
+
+
+        }
+    }
+}
---
+++ ProducerconsumerRabbitMQ/Producer/Sender.cs
@@ -0,0 +1,35 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Producer
+{
+    class Sender
+    {
+        //create factory
+        //open connection
+        //open channel
+        //declare the queue
+        //create a message
+        //publish the message
+
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName="localhost"};
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTestVatan", false, false, false, null);
+
+                string message = "Getting started with net core rabbitMQ";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish("", "BasicTestVatan", null, body);
+                Console.WriteLine("sent message {0}...",message);
+            }
+
+            Console.WriteLine("Press enter to exit the sender App...");
+            //Console.ReadLine();
+        }
+    }
+}
